cc4fd8322399d4a7276037ef5c2956516917f6a9
Asbury <misha.osinovskiy@gmail.com>
2019-12-25 10:27:19 -0800

    reorg

--- work.js
+++ work.js
@@ -1,29 +1,42 @@
+class modernTechie() {
 
-const modernTechie = () => {
-	commuteToWork();
-	doWork();
-	manageProjects();
-	eatLunch();
-	haveMeetings();
-	readEmail();
-	doYogaMeditate();
+	constructor() {
+		let name, age, experience;
+	}
+
+	function writeCode (hours, distrations) {
+		let hours = this.hours;
+		let distrations = this.distrations;
+		// let productivity = hours / 15;
+
+		linesOfCodeWritten = hours - distrations;
+		return linesOfCodeWritten;
+	}
+
+	function commuteToWork() {
+		let car, elavator, walk, scooter, homeless;
+	}
+
+	function eatLunch () {
+		let foodType, friends, service, hours;
+	}
+
+	function haveMeetings () {
+		let subject, hours, people;
+	}
+
+	function work () {
+		commuteToWork();
+		doWork();
+		manageProjects();
+		eatLunch();
+		haveMeetings();
+		readEmail();
+		doYogaMeditate();
+	}
 }
 
-function commuteToWork() {
-	let car, elavator, walk, scooter, homeless;
-}
 
-function doWork () {
+const misha = new modernTechie();
 
-}
-
-function eatLunch () {
-	let foodType, friends, service, hours;
-}
-
-function haveMeetings () {
-	let subject, hours, people;
-}
-
-
-const misha = modernTechie();
+misha.writeCode(4, 10);
